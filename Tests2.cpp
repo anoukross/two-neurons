c3e2c4b11b364f09fd015f9c20c9b8953b403586
anoukross <32436342+anoukross@users.noreply.github.com>
2017-10-22 15:32:13 +0200

    Correction bug
     provenait d'un mauvais ordre dans les arguments de connect

--- Tests2.cpp
+++ Tests2.cpp
@@ -1,7 +1,7 @@
 #include "Network.hpp"
 #include "googletest/include/gtest/gtest.h"
 #include <cmath>
-
+#include <iostream>
 //Before delay
 /*TEST (NetworkTest, VmatchesJ){
 	Network net;
@@ -24,19 +24,20 @@ TEST (NetworkTest, VmatchesD){
 		//At 92.4ms+1.5 ms=93.9 (t=924+15=939 the potential of n2 should have increased of net.getCurrentWeights()[i][j]=0.1
 		if(t<937){
 			double potential(net.getNetwork()[1]->getPotential()); 
-			net.connect(0,1,I,net.getCurrentWeights()[0][1], t);
-			net.connect(1,0,0, net.getCurrentWeights()[0][1], t);
+			net.connect(0,1,net.getCurrentWeights()[0][1], I, t);
 			EXPECT_LE((abs(potential - net.getNetwork()[1]->getPotential())), exp(-6)); //potential should be the same after connect -> delay is not over
-																					//Expect equality of double less or equal than exp(-6)		
+																					//Expect equality of double less or equal than exp(-6)	
+			net.connect(1,0, net.getCurrentWeights()[0][1], 0, t);	
 		}	
 		if(t==937){
 			double potential(net.getNetwork()[1]->getPotential()); 
-			net.connect(0,1,I,net.getCurrentWeights()[0][1], t);
+			net.connect(0,1,net.getCurrentWeights()[0][1], I, t);
+			net.connect(1,0,net.getCurrentWeights()[0][1], 0, t);
 			EXPECT_LE((abs(potential + 0.1 - net.getNetwork()[1]->getPotential())), exp(-6)); //potential corresponds to the potential before the end of the delay  	
 																//net.getNetwork()[1]->getPotential() is after receiving the amplitude J(0.1)
 																//So potential + J should = net.getNetwork()[1]->getPotential()
-			net.connect(1,0,0,net.getCurrentWeights()[0][1], t);													
-		}														
-	}	
+													
+		}
+	}
 }
 
