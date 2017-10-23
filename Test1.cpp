adeda8c3750c34cf2330f30fe6aa67fdef1d1c2c
anoukross <32436342+anoukross@users.noreply.github.com>
2017-10-23 09:20:43 +0200

    Add files via upload

--- Test1.cpp
+++ Test1.cpp
@@ -16,12 +16,15 @@ TEST (NeuronTest, shouldNotSpike){
 TEST (NeuronTest, shouldSpike){
 	Neuron n(0);
 	double I(1.01);
+	unsigned int number_of_spikes(0);
 	
-	for(unsigned int t(0); t<5000;  ++t){ //t corresponds to the step of time 5000 ->500 ms
+	for(unsigned int t(1); t<5000;  ++t){ //t corresponds to the step of time 5000 ->500 ms
 		
 		//Should spike only at 92.4ms, 186.8ms,281.2ms,375.6ms, 470.0ms
 		if((abs(92.4/Neuron::h-t) <= exp(-6)) or (abs(186.8/Neuron::h-t) <= exp(-6)) or (abs(281.2/Neuron::h-t) <= exp(-6)) or (abs(375.6/Neuron::h-t) <= exp(-6)) or (abs(470.0/Neuron::h-t) <= exp(-6))){
 			EXPECT_EQ(n.update(I,t),true); 
+			++number_of_spikes;
+				EXPECT_EQ((n.getSpikesNumber()),number_of_spikes);
 		}else{
 			EXPECT_EQ(n.update(I,t),false); 
 		}
